e34dddc51ea7255cd5704d3579b44cb1fae5badd
AbinavRavi <abinav.ravi@tum.de>
2020-05-28 00:41:21 +0200

    added median algorithm

--- median.py
+++ median.py
@@ -2,6 +2,9 @@ import torch
 import numpy as np
 import utils
 
-def median_grad(grads):
+def median_grad(grads,num_machines,num_class,num_feature):
     grad_array = np.array(grads)
-    grad_array = grad_array.reshape(num_machines
+    grad_array = grad_array.reshape(num_machines,num_class*(num_feature+1))
+    med = np.median(grad_array, axis=0)
+    med = med.reshape(num_class,num_feature+1)
+    return med
